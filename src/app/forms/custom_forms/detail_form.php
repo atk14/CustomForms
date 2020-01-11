b057d2b0f2c6656e35bb31e355c3a6ad2ac789f5
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-01-11 13:35:18 +0100

    CSRF protection is enabled automatically on small forms

--- src/app/forms/custom_forms/detail_form.php
+++ src/app/forms/custom_forms/detail_form.php
@@ -7,12 +7,18 @@ class DetailForm extends ApplicationForm {
 	function prepare_for_custom_form($custom_form){
 		global $HTTP_REQUEST;
 
+		$fields = $custom_form->getFields();
+
 		$this->set_method("post");
 		$this->set_attr("id","custom_form");
 		$this->set_action($HTTP_REQUEST->getRequestUri()."#custom_form");
 		$this->set_button_text($custom_form->getButtonText());
+		if(sizeof($fields)<=5){
+			$this->enable_csrf_protection();
+		}
 
-		foreach($custom_form->getFields() as $field){
+
+		foreach($fields as $field){
 			$class_name = $field->getClassName();
 			$options = [
 				"label" => $field->getLabel(),

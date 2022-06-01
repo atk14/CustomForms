3eea8f261efe7e1d7e76e9715a14e03e5f38e6f3
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-06-01 11:46:03 +0200

    CustomFormFields\PhoneField fixed

--- src/app/fields/custom_form_fields/phone_field.php
+++ src/app/fields/custom_form_fields/phone_field.php
@@ -8,14 +8,4 @@ namespace CustomFormFields;
  */
 class PhoneField extends \PhoneField {
 
-	function __construct($options = []){
-
-		$default_country_code = defined("ACTIVA_CORP_SK") && ACTIVA_CORP_SK ? "+421" : "+420";
-
-		$options += [
-			"default_country_code" => $default_country_code,
-		];
-
-		parent::__construct($options);
-	}
 }

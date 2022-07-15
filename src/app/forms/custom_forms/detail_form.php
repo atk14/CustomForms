01703a9ce85176eb3562b8955d463820639d272d
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-07-15 15:22:46 +0200

    A CustomForm submission should be processed neither too early (5 sec) nor too late (1 day)

--- src/app/forms/custom_forms/detail_form.php
+++ src/app/forms/custom_forms/detail_form.php
@@ -2,6 +2,9 @@
 class DetailForm extends ApplicationForm {
 
 	function set_up(){
+		global $HTTP_REQUEST;
+
+		$this->set_hidden_field("cfd_start",$HTTP_REQUEST->getPostVar("cfd_start") ? (string)$HTTP_REQUEST->getPostVar("cfd_start") : Packer::Pack(["cfd_start" => $this->_microtime()]));
 	}
 
 	function prepare_for_custom_form($custom_form){
@@ -16,7 +19,6 @@ class DetailForm extends ApplicationForm {
 		if(sizeof($fields)<=5){
 			$this->enable_csrf_protection();
 		}
-
 
 		foreach($fields as $field){
 			$class_name = $field->getClassName();
@@ -37,5 +39,51 @@ class DetailForm extends ApplicationForm {
 			}
 			$this->add_field($field->getName(),new $class_name($options));
 		}
+	}
+
+	function clean(){
+		global $HTTP_REQUEST;
+		list($err,$d) = parent::clean();
+
+		if(!$this->has_errors()){
+			$start = $this->_decode_start();
+			$current_time = $this->_microtime();
+
+			if(is_null($start) || ($current_time - $start > 60 * 60 * 24)){
+				$uri = $HTTP_REQUEST->getRequestUri();
+				$uri .= preg_match('/\?/',$uri) ? "&" : "?";
+				$uri .= "rnd=".uniqid();
+				$this->set_error(
+					_("Načtěte formulář ještě jednou, znovu jej vyplňte a odešlete")."<br><br>".
+					sprintf('<a href="%s">%s</a>',$uri."#custom_form",_("Načíst formulář znovu"))
+				);
+			}
+
+			if($current_time - $start < 5){
+				$this->set_error(_("Odešlete prosím formulář ještě jednou"));
+			}
+		}
+
+		return [$err,$d];
+	}
+
+	function _microtime(){
+		list($decimals,$time) = explode(" ",microtime());
+		$microtime = $time + $decimals;
+		return round($microtime,2);
+	}
+
+	function _decode_start(){
+		global $HTTP_REQUEST;
+
+		if(!Packer::Unpack((string)$HTTP_REQUEST->getPostVar("cfd_start"),$var)){
+			return null;
+		}
+
+		if(!is_array($var) || !isset($var["cfd_start"])){
+			return null;
+		}
+
+		return (float)$var["cfd_start"];
 	}
 }

87c2dad78a7983d672cf35b4ac8fe74b06b6000d
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-04-16 17:06:18 +0200

    DateField modernized

--- src/app/fields/custom_form_fields/date_field.php
+++ src/app/fields/custom_form_fields/date_field.php
@@ -10,8 +10,42 @@ class DateField extends \DateField {
 
 	function __construct($options = []){
 		$options += [
-			"hint" => \Atk14Locale::FormatDate("2000-12-31"),
+			"widget" => new DateInput(),
 		];
 		parent::__construct($options);
 	}
+
+	function format_initial_data($value){
+		$value = (string)$value;
+		if(preg_match('/^(\d{4}-\d{2}-\d{2})/',$value,$matches)){
+			return $matches[1];
+		}
+		return parent::format_initial_data($value);
+	}
+
+	function clean($value){
+		$value = (string)$value;
+		$value = trim($value);
+		if(preg_match('/^(\d{4}-\d{2}-\d{2})/',$value,$matches) && \Date::ByDate($matches[1])){
+			return [null,$matches[1]];
+		}
+		return parent::clean($value);
+	}
+}
+
+class DateInput extends \TextInput {
+
+	var $input_type = "date";
+
+	function value_from_datadict($data, $name){
+		if(isset($data[$name]) && is_string($data[$name])){
+			$value = trim($data[$name]);
+			if(!preg_match('/^\d{4}-\d{2}-\d{2}$/',$value)){
+				return \Atk14Locale::ParseDate($value);
+			}
+		}
+
+		return parent::value_from_datadict($data, $name);
+	}
+
 }
